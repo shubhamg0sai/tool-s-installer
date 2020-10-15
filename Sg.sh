2544bfd8c24cc39dff9e4e3f28820f849f38cdc4
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-10-15 15:12:44 +0530

    Create Sg.sh

---
+++ Sg.sh
@@ -0,0 +1,431 @@
+g='\033[1;32m'
+p='\033[1;35m'
+clear
+echo -e "\033[1;32m"
+echo "         {Ctrl C} exit "
+echo "[1] imstall "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+echo "[ ] install "
+
+
+
+
+
+
+
+echo ""
+echo -e "$p"
+read -p  "number-------> " download
+
+if [ "$download" -eq "2"  ]; then
+ cd
+ clear
+ termux-setup-storage
+ rm -rf $HOME/SG
+ rm -rf $HOME/Virus4.zip
+ rm -rf $HOME/Virus4
+ clear
+ echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'->             '$g']|'
+ sleep 0.4  
+ clear
+ echo -e $g 'Please Wait ===+['$p'-->            '$g']/'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']/'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
+ sleep 0.6
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------->      '$g']/'
+ sleep 0.6
+ clear
+ echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
+ sleep 0.6
+ clear
+ echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'----------->   '$g']/'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+ sleep 0.4
+ clear 
+ echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+ sleep 0.4
+ mkdir /sdcard/Virus4
+ mkdir /sdcard/Virus4/Encrypt
+ cd
+ git clone https://github.com/ShuBhamg0sain/SG
+ cd
+ cd SG
+ mv -v Virus4.zip $HOME
+ cd
+ rm -rf Virus
+ cd
+ unzip Virus4.zip 
+ cd Virus4 && chmod +x *
+ cd $HOME/Virus4
+ chmod +x *
+ cd
+ rm -rf SG
+ rm -rf Virus4.zip
+ cd
+ cd $HOME/Virus4/.tools
+ chmod +x */*
+ clear
+ echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
+ echo -e "     Hello      "
+ echo -e "     $p     new "
+ echo -e "     $g         Update "
+ sleep 2 
+
+ cd $HOME/Virus4
+ python2 Virus4.py
+
+elif [ "$download" -eq "1"  ]; then
+ cd
+ clear
+ termux-setup-storage
+ rm -rf $HOME/sg
+ rm -rf $HOME/Virus4.zip
+ rm -rf $HOME/Virus4
+ clear
+ echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'->             '$g']|'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-->            '$g']/'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+ sleep 0.5   
+ clear
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']/'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
+ sleep 0.6
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------->      '$g']/'
+ sleep 0.6
+ clear
+ echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
+ sleep 0.6
+ clear
+ echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'----------->   '$g']/'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+ sleep 0.4
+ clear 
+ echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+ sleep 0.4
+ cd
+ git clone https://github.com/ShuBhamg0sain/sg
+ cd sg
+ chmod +x *
+ clear
+ echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
+ echo -e "     Hello      "
+ echo -e "     $p     new "
+ echo -e "     $g         Update "
+ sleep 2
+ python2 Virus4.py
+else :
+ bash ShuBham.sh
+fi
