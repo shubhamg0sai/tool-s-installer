09fb2e94b0da4b85e124417dfe0059f8430a6ba9
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-10-16 14:23:28 +0530

    Update Sg.sh

--- Sg.sh
+++ Sg.sh
@@ -95,7 +95,7 @@ echo "    {Ctrl C} exit "
  git clone https://github.com/ShuBhamg0sain/tool-s-installer
  cd tool-s-installer
  bash Sg.sh
-echo "    {Ctrl Z} exit "
+echo "    {} exit "
  cd
  git clone https://github.com/ShuBhamg0sain/tool-s-installer
  cd tool-s-installer
@@ -103,55 +103,6 @@ echo "    {Ctrl Z} exit "
 
 
 elif [ "$install" -eq "2"  ]; then
- cd
- clear
- termux-setup-storage
- rm -rf $HOME/sg
- rm -rf $HOME/Virus4.zip
- rm -rf $HOME/Virus4
- clear
- echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'->             '$g']|'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-->            '$g']/'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
- sleep 0.5
- clear
- echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
- sleep 0.5
- clear
- echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
- sleep 0.5   
- clear
- echo -e $g 'Please Wait ===+['$p'------>        '$g']/'
- sleep 0.5
- clear
- echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
- sleep 0.6
- clear
- echo -e $g 'Please Wait ===+['$p'-------->      '$g']/'
- sleep 0.6
- clear
- echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
- sleep 0.6
- clear
- echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
- sleep 0.5
- clear
- echo -e $g 'Please Wait ===+['$p'----------->   '$g']/'
- sleep 0.4
- clear
  echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
  sleep 0.4
  clear
@@ -160,17 +111,7 @@ elif [ "$install" -eq "2"  ]; then
  clear 
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/sg
- cd sg
- chmod +x *
- clear
- echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
- echo -e "     Hello      "
- echo -e "     $p     new "
- echo -e "     $g         Update "
- sleep 2
- python2 Virus4.py
+ Ctrl+c
 else :
  bash Sg.sh
 fi
